d4052ae74813d2379864bf9f33147d3c833f2526
LahiruMadushann <lahirumadushandl@gmail.com>
2024-07-21 09:43:05 +0530

    add css to product card

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,12 +7,13 @@ interface User {
 }
 
 const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
     const users: User[] = await res.json();
     console.log("users", users)
   return (
     <div>
       UsersPage
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
