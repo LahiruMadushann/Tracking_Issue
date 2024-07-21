81af4ad4c23dee0a7244af82b23ce2820d908e31
LahiruMadushann <lahirumadushandl@gmail.com>
2024-07-21 14:40:26 +0530

    add usersinto table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import ProductCard from '../components/ProductCard'
+import React from "react";
 
 interface User {
-    id : number;
-    name: string;
+  id: number;
+  name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
-    const users: User[] = await res.json();
-    console.log("users", users)
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
   return (
     <div>
-      UsersPage
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul>
-      
-      <ProductCard />
+      <h1>Users</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
