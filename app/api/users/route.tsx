1b1ffa6c105c07feaea2a11c5b9aa53f74f3eef9
LahiruMadushann <lahirumadushandl@gmail.com>
2024-07-27 17:31:45 +0530

    implement get uers by id

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
-    return NextResponse.json('Hello')
+    return NextResponse.json([
+        { id: 1, name: 'Lahiru'},
+        { id: 2, name: 'Madushanka'}
+    ])
 }
