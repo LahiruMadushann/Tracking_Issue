da461f1937b472450ff3b593c13574072831deea
LahiruMadushann <lahirumadushandl@gmail.com>
2024-07-28 13:35:03 +0530

    fetch data from database

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: 'Lahiru'},
-        { id: 2, name: 'Madushanka'}
-    ])
+import prisma from "@/prisma/client";
+
+export async function GET(request: NextRequest) {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users)
 }
 
 export async function POST(request: NextRequest) {
