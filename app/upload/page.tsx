7f1555ced6e91b9c6053a64beabc42ecde555fcd
LahiruMadushann <lahirumadushandl@gmail.com>
2024-07-28 17:57:18 +0530

    disable multiple files allow

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,31 +1,37 @@
-'use client';
-import React, { useState } from 'react'
-import { CldUploadWidget, CldImage } from 'next-cloudinary';
+"use client";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
 
 interface CloudinaryResult {
-    public_id: string;
+  public_id: string;
 }
 
 const UploadPage = () => {
-    const [publicId, setPublicId] = useState('')
+  const [publicId, setPublicId] = useState("");
   return (
     <>
-    {publicId && 
-        <CldImage src={publicId} width={270} height={180} alt='image' />
-    }
-    <CldUploadWidget uploadPreset="ditfj9zv" onUpload = {(result,widget) =>{
-        if (result.event !== 'success') return;
-        const info = result.info as CloudinaryResult;
-        setPublicId(info.public_id);
-    }}>
-        {({ open }) => 
-            <button className='btn btn-primary' onClick={()=> open()}>
-                Upload
-            </button>
-        }
-    </CldUploadWidget>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="image" />
+      )}
+      <CldUploadWidget
+        uploadPreset="ditfj9zv"
+        options={{
+            multiple: false,
+        }}
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
     </>
-  )
-}
+  );
+};
 
-export default UploadPage
+export default UploadPage;
